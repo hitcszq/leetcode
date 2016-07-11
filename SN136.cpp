541b4796699aa9a882662ff885cb882fe995cb28
583101017@qq.com <583101017@qq.com>
2016-07-11 11:16:40 +0800

    136ac

---
+++ SN136.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int> nums_rec;
+        int nums_length=0;
+		nums_length=nums.size();
+		map<int, int>::iterator iter;
+		for(int i=0;i<nums_length;++i)
+		{
+			iter=nums_rec.find(nums[i]);
+			if(iter==nums_rec.end())
+			{
+				 nums_rec.insert(pair<int, int>(nums[i], 1));
+			}
+			else if(iter->second==1)
+			{
+				iter->second=2;
+			}
+		}
+		for(iter=nums_rec.begin();iter!=nums_rec.end();++iter)
+		{
+			if(iter->second==1)
+			{
+				return iter->first;
+			}
+		}
+		return 0;
+    }
+};
